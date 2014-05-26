819d73aa88558999e40b64d6761a87b85d7e6282
thestatsmancan26 <manserpt@vcu.edu>
2014-05-26 16:48:26 -0400

    add generics

---
+++ R/generics.R
@@ -0,0 +1,6 @@
+setGeneric("getDat", function(object) standardGeneric("getDat"))
+setGeneric("getPlatform", function(object) standardGeneric("getPlatform"))
+setGeneric("getAnnot", function(object) standardGeneric("getAnnot"))
+setGeneric("getPheno", function(object) standardGeneric("getPheno"))
+
+
