63f01d156165660729f37c3ba45860b118be1111
thestatsmancan26 <manserpt@vcu.edu>
2014-07-14 18:05:08 -0400

    switch from dplyr to split/apply to reduce dependencies

--- R/ccaTest.R
+++ R/ccaTest.R
@@ -13,14 +13,9 @@ ccaTest <- function(object, npcs = 3){
   
   # do PCA grouped by gene -------------------------------------------
   # get PC scores and redundancies
-  set1.pca <- mclapply(split(set1.df, object@set1@annot$entrez.id), pc2)
-  
-    # get PC scores for set 2
-  set2.df <- data.frame(gs = object@set2@annot$entrez.id, set2.dat)
-  set2.pca <- set2.df %.%
-    group_by(gs) %.%
-    do2(pc2)
-  
+  set1.pca <- mclapply(split(set1.df, object@set1@annot$entrez.id), pc2, npcs=npcs)
+  set2.pca <- mclapply(split(set2.df, object@set2@annot$entrez.id), pc2, npcs=npcs)
+    
   # compute canonical correlation
   cc.results <- mapply(cancor, set1.pca, set2.pca)
   
@@ -32,18 +27,15 @@ ccaTest <- function(object, npcs = 3){
                                cc.res = as.list(as.data.frame(cc.results)),
                                pca.res1 = set1.pca,
                                pca.res2 = set2.pca, 
-                               set1.dat = split(set1.dat, object@set1@annot$entrez.id),
-                               set2.dat = split(set2.dat, object@set2@annot$entrez.id))  
+                               set1.df = split(set1.df, object@set1@annot$entrez.id),
+                               set2.df = split(set2.df, object@set2@annot$entrez.id))  
   
   out <- mapply(cbind, test.results, redundancy.results)
   out <- apply(out, 2, as.data.frame)  
   out
 }
 
-pca.r2 <- function(z){
-  out <- list()
-  out$scores <- prcomp(t(z$x[ , 1:min(npcs, nrow(z))]
-                            
+pc2 <- function(z, npcs) prcomp(t(z))$x[ , 1:min(npcs, nrow(z))]                            
                             
 # lrt for each of CC pairs and choose how many CCs to keep
 cc.sig.test <- function(object, n){
@@ -57,16 +49,16 @@ cc.sig.test <- function(object, n){
 }
 
 # compute redundancy coefs
-cc.redundancy <- function(cc.res, pca.res1, pca.res2, set1.dat, set2.dat){
+cc.redundancy <- function(cc.res, pca.res1, pca.res2, set1.df, set2.df){
   # add redundancy coefficient  
   set1.scores <- pca.res1 %*% cc.res$xcoef
   set2.scores <- pca.res2 %*% cc.res$ycoef
-  set1.comm  <- cor(t(set1.dat), set1.scores)^2
-  set2.comm  <- cor(t(set2.dat), set2.scores)^2
+  set1.comm  <- cor(t(set1.df), set1.scores)^2
+  set2.comm  <- cor(t(set2.df), set2.scores)^2
   
   # redundancy index  
-  set1.v <- colVars(t(set1.dat))
-  set2.v <- colVars(t(set2.dat))
+  set1.v <- colVars(t(set1.df))
+  set2.v <- colVars(t(set2.df))
   n.ccs <- length(cc.res$cor)
   set1.r2 <- colSums((set1.v * set1.comm)/sum(set1.v))
   set2.r2 <- colSums((set2.v * set2.comm)/sum(set2.v))
