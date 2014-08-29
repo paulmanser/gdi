321874e086e0f2cbc49e82aa698d80765e46d31e
thestatsmancan26 <manserpt@vcu.edu>
2014-08-28 23:00:34 -0400

    update code for selecting entrez.ids

--- R/permTest.R
+++ R/permTest.R
@@ -1,29 +1,31 @@
 
 
-permTest <- function(object, communalities, n.perm = 1000, half.life = 300){
+permTest <- function(object, cca.results, n.perm = 1000, half.life = 300){
 
   if (!is(object, 'GDIset')) stop("object must be a 'GDIset'")
   
-  entrez.ids <- c(object@set1@annot$entrez.id, object@set2@annot$entrez.id)
-  unique.ids <- unique(entrez.ids)
+  incl.ind <- which(unlist(lapply(cca.res$testing.results, function(x) !is.na(x[1]))))
+  unique.ids <- names(cca.results$testing.results)[incl.ind]
+  # set up parallel stuff here
   
-  out <- foreach(gene=unique.ids, .packages='gdi', .combine='c') %do% {
+    
+  out <- foreach(gene=unique.ids, .packages='gdi', .combine='c') %dopar% {
     
     # get locations of sites
     set1.ind <- which(object@set1@annot$entrez.id %in% gene)
-    set1.loc <- (end(object@set1@annot[set1.ind]) - start(object@set1@annot[set1.ind]))/2
+    set1.loc <- (end(object@set1@annot[set1.ind]) + start(object@set1@annot[set1.ind]))/2
     names(set1.loc) <- names(object@set1@annot[set1.ind])
     
     set2.ind <- which(object@set2@annot$entrez.id %in% gene)
-    set2.loc <- (end(object@set2@annot[set2.ind]) - start(object@set2@annot[set2.ind]))/2
+    set2.loc <- (end(object@set2@annot[set2.ind]) + start(object@set2@annot[set2.ind]))/2
     names(set2.loc) <- names(object@set2@annot[set2.ind])
     
     # save communalities with short variable names
-    set1.comm <- communalities[[gene]]$set1
-    set2.comm <- communalities[[gene]]$set2
+    set1.comm <- cca.results$set1.loadings[[gene]][, 1, drop=FALSE]^2
+    set2.comm <- cca.results$set2.loadings[[gene]][, 1, drop=FALSE]^2
     
     # get outer product of communalities
-    comm.outer <- t(set1.comm) %*% set2.comm
+    comm.outer <- set1.comm %*% t(set2.comm)
     
     # get distance matrix 
     dist.mat <- matrix(set1.loc, nr = length(set1.loc),
